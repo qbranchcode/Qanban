3ca851daafc719bc8f07ad805423090858db3581
Mattias Mirhagen <mattias.mirhagen@gmail.com>
2009-10-28 16:52:11 +0100

    Revert "Added test values to the bootstrap in development mode ("
    This reverts commit aad55887b681c08045462d215433ee8fdcfb72cc.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,23 +1,6 @@
-import se.qbranch.qanban.*
-import grails.util.GrailsUtil
-
 class BootStrap {
 
      def init = { servletContext ->
-
-
-         switch (GrailsUtil.environment) {
-            case "development":
-                Board b = new Board()
-                            .addToPhases(new Phase(name:'Backlog'))
-                            .addToPhases(new Phase(name:'WIP'))
-                            .addToPhases(new Phase(name:'Done'))
-                            .save()
-
-
-                break
-        }
-
      }
      def destroy = {
      }
