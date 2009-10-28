aad55887b681c08045462d215433ee8fdcfb72cc
Patrik Gärdeman <patrik.gardeman@gmail.com>
2009-10-28 14:48:21 +0100

    Added test values to the bootstrap in development mode ( phase[backlog,wip,done] )

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,23 @@
+import se.qbranch.qanban.*
+import grails.util.GrailsUtil
+
 class BootStrap {
 
      def init = { servletContext ->
+
+
+         switch (GrailsUtil.environment) {
+            case "development":
+                Board b = new Board()
+                            .addToPhases(new Phase(name:'Backlog'))
+                            .addToPhases(new Phase(name:'WIP'))
+                            .addToPhases(new Phase(name:'Done'))
+                            .save()
+
+
+                break
+        }
+
      }
      def destroy = {
      }
