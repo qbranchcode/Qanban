4565d02d2f7c9354c598cc4c5e181a5e3c49fa81
Leonard Axelsson <leonard.axelsson@qbranch.se>
2009-10-29 11:35:10 +0100

    Removed incorrect constraint (blank) on Card.caseNumber

--- grails-app/domain/se/qbranch/qanban/Card.groovy
+++ grails-app/domain/se/qbranch/qanban/Card.groovy
@@ -4,7 +4,6 @@ class Card {
 
     static constraints = {
         title(blank:false, length:1..50)
-        caseNumber(blank: true)
         description(length:1..300)
         assignee(blank: true, nullable: true)
         phase(nullable: true)
