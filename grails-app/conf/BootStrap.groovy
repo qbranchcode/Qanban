e001452087bb1d93c66b6d0f09fbc6a7266a7059
Mattias Mirhagen <mattias.mirhagen@gmail.com>
2009-10-30 09:12:45 +0100

    Fixed user password md5

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,38 +1,43 @@
 import se.qbranch.qanban.*
-
+import org.apache.commons.codec.digest.DigestUtils as DU
 import grails.util.GrailsUtil
 
 class BootStrap {
 
-     def init = { servletContext ->
+    def authenticateService
 
-         switch (GrailsUtil.environment) {
+    def init = { servletContext ->
+
+        switch (GrailsUtil.environment) {
             case "development":
 
-                Role role = new Role(description:"administrator access",authority:"ROLE_ADMIN")
-                role.save()
-                Requestmap map = new Requestmap(url:"/mainView/**", configAttribute:"ROLE_ADMIN")
-                map.save()
+            Role role = new Role(description:"administrator access",authority:"ROLE_ADMIN")
+            role.save()
+            Requestmap map = new Requestmap(url:"/mainView/**", configAttribute:"ROLE_ADMIN")
+            map.save()
 
-                //TODO: Varför fungerar inte detta?
-                //User user = new User(username:"testuser",userRealName:"Mr Test",passwd:"test",enabled:true,description:"testing, testing...",email:"test@test.com",authorities:role)
-                //user.addToAuthorities(role).save()
+            def md5pass = authenticateService.passwordEncoder("test")
+            User user = new User(username:"testuser",userRealName:"Mr Test",passwd:md5pass,enabled:true,description:"testing, testing...",email:"test@test.com",authorities:role)
+            if(user.save()) {
+                println "User saved: $user"
+                role.addToPeople(user)
+            }
 
-                Phase p1 = new Phase(name:'Backlog')
-                p1.addToCards(new Card(title:"Card #1",caseNumber:1,description:'blalbblalbabla'))
-                  .addToCards(new Card(title:"Card #2",caseNumber:2,description:'blöblöblöblöbl'))
+            Phase p1 = new Phase(name:'Backlog')
+            p1.addToCards(new Card(title:"Card #1",caseNumber:1,description:'blalbblalbabla'))
+            .addToCards(new Card(title:"Card #2",caseNumber:2,description:'blöblöblöblöbl'))
 
-                Board b = new Board()
-                            .addToPhases(p1)
-                            .addToPhases(new Phase(name:'WIP'))
-                            .addToPhases(new Phase(name:'Done'))
-                            .save()
+            Board b = new Board()
+            .addToPhases(p1)
+            .addToPhases(new Phase(name:'WIP'))
+            .addToPhases(new Phase(name:'Done'))
+            .save()
 
 
-                break
+            break
         }
 
-     }
-     def destroy = {
-     }
+    }
+    def destroy = {
+    }
 } 
