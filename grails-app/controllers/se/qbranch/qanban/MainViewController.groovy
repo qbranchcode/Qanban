fbc2ebd4437cb9a7a6218db7cb8b4bd41c812451
Mattias Mirhagen <mattias.mirhagen@gmail.com>
2009-11-09 14:11:24 +0100

    Refactored MainViewController and it's tests

--- grails-app/controllers/se/qbranch/qanban/MainViewController.groovy
+++ grails-app/controllers/se/qbranch/qanban/MainViewController.groovy
@@ -35,51 +35,7 @@ class MainViewController {
         return render([result: true] as JSON)
     }
 
-    def moveCardToPosition = {
-
-        // TODO: Return something other than JSON? Maybe HTTP error code?
-
-        if( !params.id || !params.moveTo )
-        return render([result: false] as JSON)
-
-        def moveTo = params.moveTo as Integer
-        def card = Card.get(params.id)
-        def cards = card.phase.cards
-
-        if(moveTo != null && card && moveTo < cards.size()) {
-            def oldCardIndex = cards.indexOf(card)
-            cards.remove(oldCardIndex)
-            cards.add(moveTo, card)
-            return render([result: true] as JSON)
-        }
-        
-        render([result: false] as JSON)
-
-    }
-
     def showBoard = {
         render(template: "/board/board", bean: Board.get(1))
-    }
-
-    def moveCardToPhase = {
-        if (!params.id || !params.moveTo){
-            return render([result: false] as JSON)
-        }
-
-        def moveTo = params.moveTo as Integer
-        def card = Card.get(params.id)
-        def phases = card.phase.board.phases
-        def allowedPhase = card.phase.board.phases.indexOf(card.phase).plus(2)
-        def newPhaseIndex = allowedPhase.minus(1)
-        def board = Board.get(1)
-        def cardsInPhase = board.phases[newPhaseIndex].cards.size()
-        def cardLimit = board.phases[newPhaseIndex].cardLimit
-
-        if(moveTo != null && card && moveTo < phases.size() && moveTo == allowedPhase && cardsInPhase < cardLimit ) {
-            card.phase.cards.remove(card)
-            board.phases[newPhaseIndex].cards.add(0, card)
-            return render([result: true] as JSON)
-        }
-        render([result: false] as JSON)
     }
 }
--- test/unit/se/qbranch/qanban/MainViewControllerTests.groovy
+++ test/unit/se/qbranch/qanban/MainViewControllerTests.groovy
@@ -5,50 +5,62 @@ import grails.converters.*
 
 class MainViewControllerTests extends ControllerUnitTestCase {
 
-    def b
+    def board
+    def firstPhase
+    def secondPhase
+    def topCardInFirstPhase
+    def middleCardInFirstPhase
+    def bottomCardInFirstPhase
+
 
     protected void setUp() {
         super.setUp()
-        mockDomain(Card, [ new Card(title: "TestCard",
-                    description: "This is a description",
-                    caseNumber: 1),
-
-                new Card(title: "OtherCard",
-                    description: "This is the other card",
-                    caseNumber: 2),
-                new Card(title: "Card three",
-                    description: "This is the third card",
-                    caseNumber: 5)])
+        mockDomain(Card, [ new Card(title: "Card1",
+                           description: "This is the first card",
+                           caseNumber: 1),
+                           new Card(title: "Card2",
+                           description: "This is the second card",
+                           caseNumber: 2),
+                           new Card(title: "Card3",
+                           description: "This is the third card",
+                           caseNumber: 3)])
   
-        mockDomain(Board)
-        mockDomain(Phase)
+        mockDomain(Phase, [new Phase(name: "Phase1", cardLimit: 5),
+                           new Phase(name: "Phase2", cardLimit: 3)])
 
-        b = new Board().addToPhases(new Phase(name: "test", cardLimit: 5))
-        .addToPhases(new Phase(name: "other phase", cardLimit: 3))
-        .addToPhases(new Phase(name: "thid phase", cardLimit: 3))
+        mockDomain(Board, [new Board()])
+        
+        board = Board.get(1)
+        firstPhase = Phase.findByName("Phase1")
+        secondPhase = Phase.findByName("Phase2")
+        topCardInFirstPhase = Card.findByTitle("Card1")
+        middleCardInFirstPhase = Card.findByTitle("Card2")
+        bottomCardInFirstPhase = Card.findByTitle("Card3")
+        
+        board
+        .addToPhases(firstPhase)
+        .addToPhases(secondPhase)
         .save()
+        
+        firstPhase
+        .addToCards(topCardInFirstPhase)
+        .addToCards(middleCardInFirstPhase)
+        .addToCards(bottomCardInFirstPhase)
+        .save()
+
+
         for(card in Card.list()) {
-            def phase = b.phases[0]
-            def p2 = b.phases[1]
-            def p3 = b.phases[2]
-            phase.cards.add(card)
-            card.phase = phase
-            card.phase.board = b
-            p2.board = b
-            p3.board = b
-            p2.save()
-            p3.save()
-            phase.save()
+            card.phase = firstPhase
+            card.phase.board = board
             card.save()
         }
-
 
 
     }
 
     void testSetup() {
         assertEquals 1,Board.list().size()
-        assertEquals 3,Phase.list().size()
+        assertEquals 2,Phase.list().size()
         assertEquals 3,Card.list().size()
 
     }
@@ -57,91 +69,43 @@ class MainViewControllerTests extends ControllerUnitTestCase {
         super.tearDown()
     }
 
-    void testMoveCardSuccess() {
-
-        // Ta card med id 3 och flytta från pos 2 till pos 0
-        // om det funkar får man true och domänmodellen ser annorlunda ut, verifiera
-
-        // Making request to moveCard
-        mockParams.id = "3"
-        mockParams.moveTo = "0"
-        controller.moveCardToPosition()
-        def response = JSON.parse(controller.response.contentAsString)
-        assertTrue "Expected move to return true", response.result
-
-        assertEquals 3, b.phases[0].cards[0].id
-        assertEquals 3, b.phases[0].cards.size()
-
-        
-    }
-
-    void testMoveCardToIllegalPosition() {
-        mockParams.id = 3
-        mockParams.moveTo = 3
-        controller.moveCardToPosition()
-        def response = JSON.parse(controller.response.contentAsString)
-        assertFalse "Expected move to return false", response.result
-
-    }
-
-    void testNotSettingMoveTo() {
-        mockParams.id = 3
-        controller.moveCardToPosition()
-        def response = JSON.parse(controller.response.contentAsString)
-        assertFalse "Expected move to return false", response.result
-    }
-
-    void testMoveCardToAllowedPhase() {
-        mockParams.id = 3
-        mockParams.moveTo = 2
-        controller.moveCardToPhase()
-        def response = JSON.parse(controller.response.contentAsString)
-        assertTrue "Expected move to return true", response.result
-        assertEquals 3, b.phases[1].cards[0].id
-    }
-
     void testMoveCardToPhaseThatDoesntExists() {
-        mockParams.id = 3
-        mockParams.moveTo = 7
-        controller.moveCardToPhase()
+        mockParams.id = "3"
+        mockParams.movePhase = "7"
+        mockParams.movePosition = "0"
+        controller.moveCard()
         def response = JSON.parse(controller.response.contentAsString)
         assertFalse "Expected move to return false", response.result
     }
 
     void testMoveCardMoreThanOnePhase() {
-        mockParams.id = 3
-        mockParams.moveTo = 3
-        controller.moveCardToPhase()
+        mockParams.id = "3"
+        mockParams.movePhase = "3"
+        mockParams.movePosition = "0"
+        controller.moveCard()
         def response = JSON.parse(controller.response.contentAsString)
         assertFalse "Expected move to return false", response.result
     }
 
-    void testMoveCardBackwards() {
+    void testNotSettingPhaseMovePhaseOrMovePosition() {
         mockParams.id = 3
-        mockParams.moveTo = 3
-        controller.moveCardToPhase()
-        def response = JSON.parse(controller.response.contentAsString)
-        assertFalse "Expected move to return false", response.result
-    }
-
-    void testNotSettingPhaseMoveTo() {
-        mockParams.id = 3
-        controller.moveCardToPhase()
+        controller.moveCard()
         def response = JSON.parse(controller.response.contentAsString)
         assertFalse "Expected move to return false", response.result
     }
 
     void testMoveCardToFullPhase() {
-        b.phases[1].addToCards(new Card(title: "p1c1", description: "p1c1DESC", caseNumber: 9))
-        b.phases[1].addToCards(new Card(title: "p1c2", description: "p1c2DESC", caseNumber: 8))
-        b.phases[1].addToCards(new Card(title: "p1c3", description: "p1c3DESC", caseNumber: 7))
-        b.save()
-        mockParams.id = 3
-        mockParams.moveTo = 2
-        controller.moveCardToPhase()
-        b.save()
-
-        assertEquals 3, b.phases[1].cards.size()
+        secondPhase.addToCards(new Card(title: "p1c1", description: "p1c1DESC", caseNumber: 9))
+        secondPhase.addToCards(new Card(title: "p1c2", description: "p1c2DESC", caseNumber: 8))
+        secondPhase.addToCards(new Card(title: "p1c3", description: "p1c3DESC", caseNumber: 7))
+        secondPhase.save()
+        
+        mockParams.id = "3"
+        mockParams.movePhase = "2"
+        mockParams.movePosition = "0"
+        controller.moveCard()
+        
+        assertEquals 3, secondPhase.cards.size()
         def response = JSON.parse(controller.response.contentAsString)
         assertFalse "Expected move to return false", response.result
     }
@@ -149,19 +113,19 @@ class MainViewControllerTests extends ControllerUnitTestCase {
     void testMoveCardInPhaseAndToNewPhase() {
         mockParams.id = "3"
         mockParams.movePosition = "0"
-        mockParams.movePhase = "1"
-
+        mockParams.movePhase = "2"
         controller.moveCard()
+        
         def response = JSON.parse(controller.response.contentAsString)
         assertTrue "Expected move to return true", response.result
-        assertEquals 1, b.phases[1].cards.size()
+        assertEquals 1, secondPhase.cards.size()
     }
 
     void testMoveCardBackPhase() {
         def card = new Card(title: "p1c3", description: "p1c3DESC", caseNumber: 7)
-        b.phases[2].addToCards(card)
-        b.save()
-        card.phase = b.phases[1]
+        secondPhase.addToCards(card)
+        secondPhase.save()
+        card.phase = secondPhase
         card.save()
 
         mockParams.id = "4"
@@ -171,18 +135,7 @@ class MainViewControllerTests extends ControllerUnitTestCase {
         controller.moveCard()
         def response = JSON.parse(controller.response.contentAsString)
         assertFalse "Expected move to return false", response.result
-        assertEquals 3, b.phases[0].cards.size()
+        assertEquals 3, firstPhase.cards.size()
     }
-
-//    void testMoveCardTwoPhases() {
-//        mockParams.id = "3"
-//        mockParams.movePosition = "0"
-//        mockParams.movePhase = "2"
-//
-//        controller.moveCard()
-//        def response = JSON.parse(controller.response.contentAsString)
-//        assertFalse "Expected move to return false", response.result
-//        assertEquals 3, b.phases[0].cards.size()
-//    }
     
 }
