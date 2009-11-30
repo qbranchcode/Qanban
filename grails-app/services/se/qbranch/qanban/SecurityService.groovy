c3d41572bca118e043b1a95d2f186d24251f5e75
Patrik Gärdeman <patrik.gardeman@gmail.com>
2009-11-30 08:53:45 +0100

    Added a service with acegi related methods

---
+++ grails-app/services/se/qbranch/qanban/SecurityService.groovy
@@ -0,0 +1,38 @@
+package se.qbranch.qanban
+
+import org.springframework.security.context.SecurityContextHolder as SCH
+import org.codehaus.groovy.grails.plugins.springsecurity.AuthorizeTools
+
+class SecurityService {
+
+    boolean transactional = true
+
+    def getLoggedInUser() {
+        def user = null
+        if (isAuthenticated()) {
+            user = User.get(determineSource().id)
+        }
+        return user
+    }
+
+    private def determineSource() {
+        def principal = SCH.context.authentication.principal
+        def source
+
+        // check to see if it's a GrailsUser/GrailsUserImpl/subclass,
+        // or otherwise has a 'domainClass' property
+        if (principal.metaClass.respondsTo(principal, 'getDomainClass')) {
+                source = principal.domainClass
+        }
+        if (!source) {
+                source = principal
+        }
+
+        return source
+    }
+
+    private boolean isAuthenticated() {
+        def authPrincipal = SCH?.context?.authentication?.principal
+        return authPrincipal != null && authPrincipal != 'anonymousUser'
+    }
+}
