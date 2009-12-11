a8749c2f757918c94a185766475e7f259ef36617
Patrik Gärdeman <patrik.gardeman@gmail.com>
2009-12-11 12:10:26 +0100

    Refactored the log workflow and changed the default data to be shown in a descending order by the date of the events

--- grails-app/controllers/se/qbranch/qanban/MainViewController.groovy
+++ grails-app/controllers/se/qbranch/qanban/MainViewController.groovy
@@ -5,26 +5,36 @@ import org.codehaus.groovy.grails.plugins.springsecurity.Secured
 @Secured(['IS_AUTHENTICATED_FULLY'])
 class MainViewController {
 
-    def securityService
-    def index = { redirect(action:view,params:params)  }
+  def securityService
+  def index = { redirect(action:view,params:params)  }
 
-    def view = {
-        [ board : Board.get(1), loggedInUser: securityService.getLoggedInUser() ]
-    }
+  def view = {
+    [ board : Board.get(1), loggedInUser: securityService.getLoggedInUser() ]
+  }
 
-    def showBoard = {
-        render(template: "/board/board", bean: Board.get(1))
-    }
+  def showBoard = {
+    render(template: "/board/board", bean: Board.get(params.'board.id'))
+  }
 
-    def showLog = {
-        params.max = Math.min( params.max ? params.max.toInteger() : 40,  100)
-        render(template: "/event/log", model: [ eventInstanceList: Event.list( params ), eventInstanceTotal: Event.count() ])
-    }
+  def showLog = {
+    // This action needs the same model as showLogBody because it render the default body content through a template
+    params.max = params.max ? params.max as Integer : 40
+    params.order = params.order ? params.order : 'desc'
+    params.sort = 'dateCreated'
 
-    def showLogBody = {
-        params.max = Math.min( params.max ? params.max.toInteger() : 40,  100)
-        render(template: "/event/logBody", model: [ eventInstanceList: Event.list( params ), eventInstanceTotal: Event.count() , offset : params.offset as Integer ])
-    }
+    // TODO: Only get the events connected to the current board
+    render(template: "/event/log", model: [ eventInstanceTotal: Event.count(), eventInstanceList: Event.list( params ), board: Board.get(params.'board.id') ])
+  }
+
+  def showLogBody = {
+    params.max = params.max ? params.max as Integer : 40
+    params.order = params.order ? params.order : 'desc'
+
+    // TODO: Only get the events connected to the current board
+    def board = Board.get(params.'board.id')
+
+    render(template: "/event/logBody", model: [ eventInstanceList: Event.list( params ), offset : params.offset as Integer ])
+  }
 
 }
 
