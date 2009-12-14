a7cf1eb67f829f24e28fe25ea9ca9a16df57d0ca
Patrik Gärdeman <patrik.gardeman@gmail.com>
2009-12-14 17:16:10 +0100

    Added some tests to CardControllerTests.

--- test/unit/se/qbranch/qanban/CardControllerTests.groovy
+++ test/unit/se/qbranch/qanban/CardControllerTests.groovy
@@ -20,180 +20,240 @@ import grails.test.*
 
 class CardControllerTests extends ControllerUnitTestCase {
 
-    def eventServiceMock
-    def securityServiceMock
+  def eventServiceMock
+  def securityServiceMock
 
-    def user1
-    def user2
+  def user1
+  def user2
 
-    def board
+  def board
 
-    def phase1
-    def phase2
-    def phase3
+  def phase1
+  def phase2
+  def phase3
 
-    def card1onPhase1
-    def card2onPhase1
-    def card3onPhase2
-
-
-     protected void setUp() {
-        super.setUp()
-
-        // User mock
-
-        user1 = new User(username: "opsmrkr01", userRealName: "Mr. Krister")
-        user2 = new User(username: "opsshba01", userRealName: "Shean Banan")
-
-        mockDomain(User,[user1,user2])
+  def card1onPhase1
+  def card2onPhase1
+  def card3onPhase2
 
 
-        // Board mock
+  protected void setUp() {
+    super.setUp()
 
-        board = new Board()
-        mockDomain(Board,[board])
+    // User mock
+
+    user1 = new User(username: "opsmrkr01", userRealName: "Mr. Krister")
+    user2 = new User(username: "opsshba01", userRealName: "Shean Banan")
+
+    mockDomain(User,[user1,user2])
 
 
-        // Phase / PhaseEventCreate mock
+    // Board mock
 
-        mockDomain(PhaseEventCreate)
-        mockDomain(Phase)
+    board = new Board()
+    mockDomain(Board,[board])
 
-        def phaseEventCreate1 = new PhaseEventCreate(title: "First phase", cardLimit: 5, phasePos: 0, user: user1, board: board)
-        def phaseEventCreate2 = new PhaseEventCreate(title: "Second phase", cardLimit: 10, phasePos: 1, user: user1 , board: board)
-        def phaseEventCreate3 = new PhaseEventCreate(title: "Third phase", user: user1, phasePos: 2, board: board)
 
-        phaseEventCreate1.beforeInsert()
-        phaseEventCreate1.save()
-        phaseEventCreate1.process()
+    // Phase / PhaseEventCreate mock
+    mockDomain(Event)
+    mockDomain(PhaseEventCreate)
+    mockDomain(Phase)
 
-        phaseEventCreate2.beforeInsert()
-        phaseEventCreate2.save()
-        phaseEventCreate2.process()
+    def phaseEventCreate1 = new PhaseEventCreate(title: "First phase", cardLimit: 5, phasePos: 0, user: user1, board: board)
+    def phaseEventCreate2 = new PhaseEventCreate(title: "Second phase", cardLimit: 10, phasePos: 1, user: user1 , board: board)
+    def phaseEventCreate3 = new PhaseEventCreate(title: "Third phase", user: user1, phasePos: 2, board: board)
 
-        phaseEventCreate3.beforeInsert()
-        phaseEventCreate3.save()
-        phaseEventCreate3.process()
+    phaseEventCreate1.beforeInsert()
+    phaseEventCreate1.save()
+    phaseEventCreate1.process()
 
-        phase1 = phaseEventCreate1.phase
-        phase2 = phaseEventCreate2.phase
-        phase3 = phaseEventCreate3.phase
+    phaseEventCreate2.beforeInsert()
+    phaseEventCreate2.save()
+    phaseEventCreate2.process()
 
-        assertEquals phase1, Phase.findByDomainId(phase1.domainId)
+    phaseEventCreate3.beforeInsert()
+    phaseEventCreate3.save()
+    phaseEventCreate3.process()
 
-        // Card / CardEventCreate mock
+    phase1 = phaseEventCreate1.phase
+    phase2 = phaseEventCreate2.phase
+    phase3 = phaseEventCreate3.phase
 
-        mockDomain(CardEventCreate)
-        mockDomain(Card)
+    assertEquals phase1, Phase.findByDomainId(phase1.domainId)
 
-        def cardEventCreate1 = new CardEventCreate(title:"Card #1",caseNumber:1,description:"The first card originally from First phase on the first position",phaseDomainId:phase1.domainId,user:user1)
-        def cardEventCreate2 = new CardEventCreate(title:"Card #2",caseNumber:2,description:"The second card originally from First phase on the second position",phaseDomainId:phase1.domainId,user:user1)
-        def cardEventCreate3 = new CardEventCreate(title:"Card #3",caseNumber:3,description:"The third card originally from Second phase on the first position",phaseDomainId:phase2.domainId,user:user1)
+    // Card / CardEventCreate mock
 
-        cardEventCreate1.beforeInsert()
-        cardEventCreate1.save()
-        cardEventCreate1.process()
+    mockDomain(CardEventCreate)
+    mockDomain(Card)
 
-        cardEventCreate2.beforeInsert()
-        cardEventCreate2.save()
-        cardEventCreate2.process()
+    def cardEventCreate1 = new CardEventCreate(title:"Card #1",caseNumber:1,description:"The first card originally from First phase on the first position",phaseDomainId:phase1.domainId,user:user1)
+    def cardEventCreate2 = new CardEventCreate(title:"Card #2",caseNumber:2,description:"The second card originally from First phase on the second position",phaseDomainId:phase1.domainId,user:user1)
+    def cardEventCreate3 = new CardEventCreate(title:"Card #3",caseNumber:3,description:"The third card originally from Second phase on the first position",phaseDomainId:phase2.domainId,user:user1)
 
-        cardEventCreate3.beforeInsert()
-        cardEventCreate3.save()
-        cardEventCreate3.process()
+    cardEventCreate1.beforeInsert()
+    cardEventCreate1.save()
+    cardEventCreate1.process()
 
-        card1onPhase1 = cardEventCreate1.card
-        card2onPhase1 = cardEventCreate2.card
-        card3onPhase2 = cardEventCreate3.card
+    cardEventCreate2.beforeInsert()
+    cardEventCreate2.save()
+    cardEventCreate2.process()
 
-        // Assertions to validate the mock setup
+    cardEventCreate3.beforeInsert()
+    cardEventCreate3.save()
+    cardEventCreate3.process()
 
-        board.phases.each {
-            println it
-            it.cards.each {
-                println "   $it"
-            }
-        }
+    card1onPhase1 = cardEventCreate1.card
+    card2onPhase1 = cardEventCreate2.card
+    card3onPhase2 = cardEventCreate3.card
 
-        assertEquals 1, board.id
-        assertEquals 3, board.phases.size()
-        assertEquals 1, phase1.id
-        assertEquals 2, phase1.cards.size()
-        assertEquals 2, phase2.id
-        assertEquals 1, phase2.cards.size()
-        assertEquals 3, phase3.id
-        assertEquals 1, card1onPhase1.id
-        assertEquals 2, card2onPhase1.id
-        assertEquals 3, card3onPhase2.id
+    // Assertions to validate the mock setup
 
-        securityServiceMock = mockFor(SecurityService)
-        securityServiceMock.demand.getLoggedInUser() { -> return user1 }
-        controller.securityService = securityServiceMock.createMock()
-
-        eventServiceMock = mockFor(EventService)
-        eventServiceMock.demand.persist() { event ->
-            event.beforeInsert()
-            if( event.save() ){
-                event.process()
-            }
-        }
-        
-        controller.eventService = eventServiceMock.createMock()
-
+    board.phases.each {
+      println it
+      it.cards.each {
+        println "   $it"
+      }
     }
 
-    protected void tearDown() {
-        super.tearDown()
+    assertEquals 1, board.id
+    assertEquals 3, board.phases.size()
+    assertEquals 1, phase1.id
+    assertEquals 2, phase1.cards.size()
+    assertEquals 2, phase2.id
+    assertEquals 1, phase2.cards.size()
+    assertEquals 3, phase3.id
+    assertEquals 1, card1onPhase1.id
+    assertEquals 2, card2onPhase1.id
+    assertEquals 3, card3onPhase2.id
+
+    securityServiceMock = mockFor(SecurityService)
+    securityServiceMock.demand.getLoggedInUser() { -> return user1 }
+    controller.securityService = securityServiceMock.createMock()
+
+    eventServiceMock = mockFor(EventService)
+    eventServiceMock.demand.persist() { event ->
+      event.beforeInsert()
+      if( event.save() ){
+        event.process()
+      }
     }
 
-    void testCardShouldNotSaveWithoutBoardId(){
+    controller.eventService = eventServiceMock.createMock()
 
-        mockParams.title = "Title"
-        mockParams.caseNumber =  "1"
-        mockParams.description = "My testcard"
+    mockForConstraintsTests(SetAssigneeCommand)
 
-        def model = controller.create()
-        println "status: $renderArgs.status"
-        assertEquals 400, renderArgs.status
+  }
 
-    }
+  protected void tearDown() {
+    super.tearDown()
+  }
 
-    void testCardShouldSave() {
-        
-        mockParams.title = "Title"
-        mockParams.caseNumber =  "1"
-        mockParams.description = "My testcard"
-        mockParams.boardId = "1"
+  void testCardShouldNotSaveWithoutBoardId(){
 
-        def model = controller.create()
-        
-        def card = Card.findByTitle("Title")
+    mockParams.title = "Title"
+    mockParams.caseNumber =  "1"
+    mockParams.description = "My testcard"
 
-        assertEquals "My testcard" , card.description
-        assertEquals "First phase", card.phase.title
-        assertEquals "Title", Phase.get(1).cards[2].title
-        assertEquals card.caseNumber, CardEventCreate.findByDomainId(card.domainId).caseNumber
-    }
+    def model = controller.create()
+    println "status: $renderArgs.status"
+    assertEquals 400, renderArgs.status
 
+  }
 
-//    void testSaveWithEmptyForm() {
-//        def cmd = new CreateCardCommand()
-//        cmd.validate()
-//        controller.create(cmd);
-//        assertEquals true, cmd.hasErrors()
-//    }
-//
-//    void testShowMethodWithoutId() {
-//        controller.show()
-//        assertEquals 400, renderArgs.status
-//    }
-//
-//    void testShowMethodWithUnexistingId() {
-//        mockParams.id = "220"
-//        controller.show()
-//        assertEquals 404, renderArgs.status
-//    }
+  void testCardShouldSave() {
 
+    mockParams.title = "Title"
+    mockParams.caseNumber =  "1"
+    mockParams.description = "My testcard"
+    mockParams.boardId = "1"
+
+    def model = controller.create()
+
+    def card = Card.findByTitle("Title")
+
+    assertEquals "My testcard" , card.description
+    assertEquals "First phase", card.phase.title
+    assertEquals "Title", Phase.get(1).cards[2].title
+    assertEquals card.caseNumber, CardEventCreate.findByDomainId(card.domainId).caseNumber
+  }
+
+  void testShowCard() {
+    mockParams.id = "1"
+    controller.show()
+    assertEquals 'card', renderArgs.template
+    assertEquals 1, renderArgs.bean.id
+
+  }
+
+  void testShowWithoutId() {
+    def model = controller.show()
+    assertEquals 400, renderArgs.status
+  }
+
+  void testShowWithInvalidId() {
+    mockParams.id = "6578921312"
+    controller.show()
+    assertEquals 404, renderArgs.status
+  }
+
+  void testFormWithCardId(){
+    mockParams.id = "1"
+    controller.form()
+    assertEquals Card.get(1).title, renderArgs.model.updateEvent.title
+  }
+
+  void testFormWithoutIdAndWithBoardId(){
+    mockParams.'board.id' = '1'
+    def model = controller.form()
+    assertEquals 1, renderArgs.model.boardInstance.id
+  }
+
+  void testFormWithoutIdAndWithoutBoardId(){
+    controller.form()
+    assertEquals 400, renderArgs.status
+  }
+
+  void testFormWithInvalidId() {
+    mockParams.id = "6578921312"
+    controller.form()
+    assertEquals 404, renderArgs.status
+  }
+
+  void testFormWithMoveParams(){
+    mockParams.id = "1"
+    mockParams.newPhase = "2"
+    mockParams.newPos = "0"
+
+    controller.form()
+
+    assertEquals Card.get(1).title, renderArgs.model.moveEvent.card.title
+    assertEquals '2', renderArgs.model.newPhase
+    assertEquals '0', renderArgs.model.newPos
+    
+  }
+
+  /*
+    Controller actions that needs both params and command object doesn't seem to be testable in unit test?
+  void testValidUpdate(){
+
+    mockParams.id = "1"
+    mockParams.title = "New Title"
+
+    def cmd = new SetAssigneeCommand(id: "1", assigneeId: "2")
+    cmd.validate()
+    controller.update(cmd)
+
+    assertEquals "New Title", renderArgs.model.updateEvent.card.title
+
+  }
+  */
+
+  void testDelete(){
+    mockParams.id = "1"
+    assertNotNull "Card with id 1 should exist", Card.get(1)
+    controller.delete()
+    assertNull "Card with id 1 should not exist", Card.get(1) 
+  }
 
 
 
