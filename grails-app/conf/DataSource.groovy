ecaa8a7c5168c3686a8f11e8252979f5b1bee920
Mattias Mirhagen <mattias.mirhagen@gmail.com>
2009-12-18 12:24:30 +0100

    Now checks for JNDI jdbc/qanban, if non existant, use HSQLDB

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,18 +1,21 @@
+import javax.naming.InitialContext
+import javax.naming.NameNotFoundException
+
 /*
- * Copyright 2009 Qbranch AB
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Copyright 2009 Qbranch AB
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 dataSource {
     pooled = true
@@ -29,8 +32,16 @@ hibernate {
 environments {
     development {
         dataSource {
+          try {
+            InitialContext ctx = new InitialContext()
+            javax.sql.DataSource ds = (javax.sql.DataSource) ctx.lookup( "java:comp/env/jdbc/qanban" )
+
+            dbCreate = "update"
+            jndiName = "java:comp/env/jdbc/qanban"
+          } catch(NameNotFoundException e) {
             dbCreate = "create-drop"
             url = "jdbc:hsqldb:file:qanbanDevDb;shutdown=true"
+          }
         }
     }
     test {
@@ -41,8 +52,19 @@ environments {
     }
     production {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:hsqldb:file:qanbanProdDb;shutdown=true"
-        }
+          //IF (JNDI) USE JNDI
+          //ELSE
+          //jndiName = "java:comp/env/jdbc/qanban
+          try {
+              InitialContext ctx = new InitialContext()
+              javax.sql.DataSource ds = (javax.sql.DataSource) ctx.lookup( "java:comp/env/jdbc/qanban" )
+
+              dbCreate = "update"
+              jndiName = "java:comp/env/jdbc/qanban"
+            } catch(NameNotFoundException e) {
+              dbCreate = "update"
+              url = "jdbc:hsqldb:file:qanbanProdDb;shutdown=true"
+            }
+          }
     }
 }
