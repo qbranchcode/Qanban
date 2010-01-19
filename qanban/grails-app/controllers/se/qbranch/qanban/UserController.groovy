e4c540a1273e01eea5de02cd40c3b17ab9870d58
Patrik Gärdeman <patrik.gardeman@gmail.com>
2010-01-19 11:15:46 +0100

    Cleaned up the user controller - removed unused generated actions

--- qanban/grails-app/controllers/se/qbranch/qanban/UserController.groovy
+++ qanban/grails-app/controllers/se/qbranch/qanban/UserController.groovy
@@ -41,7 +41,6 @@ class UserController {
     createEvent = new UserEventCreate(user:user)
     createEvent.populateFromUser()
 
-
     eventService.persist(createEvent)
 
     if ( !createEvent.hasErrors()) {
@@ -56,126 +55,60 @@ class UserController {
     return render(template: '/login/register' , model: [ person : createEvent.user ])
   }
 
-  // Retrieve
-  @Secured(['IS_AUTHENTICATED_FULLY'])
-  def show = {
-    def person = User.get(params.id)
-    if (!person) {
-      flash.message = "User not found with id $params.id"
-      redirect action: list
-      return
-    }
-    List roleNames = Role.list()*.authority
 
-
-    roleNames.sort { r1, r2 ->
-      r1 <=> r2
-    }
-
-    render(template:'userForm', model: [person: person, roleNames: roleNames])
-  }
-
-  @Secured(['IS_AUTHENTICATED_FULLY'])
-  def list = {
-    if (!params.max) {
-      params.max = 10
-    }
-    [personList: User.list(params)]
-  }
-
-  @Secured(['IS_AUTHENTICATED_FULLY'])
-  def showOnlineUsers = {
-    def users = sessionRegistry.getAllPrincipals()
-    def onlineUsers = []
-
-    for(user in users) {
-      def userObject = User.findByUsername(user)
-      onlineUsers.add(userObject)
-    }
-
-    render(template:'onlineUsers',model:[onlineUsers:onlineUsers])
-  }
 
   @Secured(['IS_AUTHENTICATED_FULLY'])
   def passForm = {
-    render(template:'passwordForm', model: [id:params.id])
+
+    if ( !params.id )
+      return render(status: 400, text: "id have to be specified")
+    
+    def user = User.get(params.id)
+
+    if( !user )
+      return render(status: 404, text: "User with id $params.id not found")
+
+    def updateEvent = createUserEventUpdate(user,null)
+
+    render(template:'passwordForm', model: [ event: updateEvent ])
+    
   }
-
-
-
-
-  /**
-   * Person delete action. Before removing an existing person,
-   * he should be removed from those authorities which he is involved.
-   */
-  @Secured(['IS_AUTHENTICATED_FULLY'])
-  def delete = {
-
-    def person = User.get(params.id)
-    if (person) {
-      def authPrincipal = authenticateService.principal()
-      //avoid self-delete if the logged-in user is an admin
-      if (!(authPrincipal instanceof String) && authPrincipal.username == person.username) {
-        flash.message = "You can not delete yourself, please login as another admin and try again"
-      }
-      else {
-        //first, delete this person from People_Authorities table.
-        Role.findAll().each { it.removeFromPeople(person) }
-        person.delete()
-        flash.message = "User $params.id deleted."
-      }
-    }
-    else {
-      flash.message = "User not found with id $params.id"
-    }
-
-    redirect action: list
-  }
-
-
-
-
-
-
-
 
 
   @Secured(['IS_AUTHENTICATED_FULLY'])
   def form = {
     if( !params.id )
-        return renderFormCreateMode(params)
+      return renderFormCreateMode(params)
 
     def person = User.get(params.id)
 
     if( !person )
-        return render(status: 404, text: "User with id $params.id not found")
+      return render(status: 404, text: "User with id $params.id not found")
 
     return renderFormEditMode(person)
-    
+
   }
 
-  private def renderFormCreateMode(params){
+  private renderFormCreateMode(params){
     return render( status:500, text: "Rendering form for creation not yet implemented")
   }
 
-  private def renderFormEditMode(person){
-      def updateEvent = new UserEventUpdate(user: person)
-      updateEvent.populateFromUser()
-
-      def roleNames = getUserRoles(person)
-
-      return render(template:'userForm',model:[ event: updateEvent, roleNames: roleNames ])
+  private renderFormEditMode(person){
+    def updateEvent = new UserEventUpdate(user: person)
+    updateEvent.populateFromUser()
+    def roleNames = getUserRoles(person)
+    return render(template:'userForm',model:[ event: updateEvent, roleNames: roleNames ])
   }
 
   private getUserRoles(user){
-      def roleNames = user.authorities*.authority
-
-      roleNames.sort { r1, r2 ->
-        r1 <=> r2
-      }
-    
-      return roleNames
+    def roleNames = user.authorities*.authority
+    roleNames.sort { r1, r2 ->
+      r1 <=> r2
+    }
+    return roleNames
   }
+
+
   /**
    * Person update action.
    */
@@ -184,25 +117,26 @@ class UserController {
     def person = User.get(params.id)
 
     if( !person )
-      return render(status: 404, text: "User with id $params.id not found")
+    return render(status: 404, text: "User with id $params.id not found")
 
     def updateEvent = createUserEventUpdate(person, params)
     eventService.persist(updateEvent)
 
-    return renderUpdateResults(updateEvent)
+    return renderUpdateResults(updateEvent,params)
   }
 
-  private renderUpdateResults(updateEvent){
+  private renderUpdateResults(updateEvent, params){
     withFormat{
       html{
-          def roleNames = getUserRoles(updateEvent.user)
-          return render( template: 'userForm', model: [ event: updateEvent, roleNames: roleNames ])
+        def roleNames = getUserRoles(updateEvent.user)
+        def template = params.template ? params.template : 'userForm'
+        return render( template: template, model: [ event: updateEvent, roleNames: roleNames ])
       }
       js{
-          return render ( [ userInstance: updateEvent.user ] as JSON )
+        return render ( [ userInstance: updateEvent.user ] as JSON )
       }
       xml{
-          return render ( [ userInstence: updateEvent.user ] as XML )
+        return render ( [ userInstence: updateEvent.user ] as XML )
       }
     }
   }
@@ -210,37 +144,13 @@ class UserController {
   private createUserEventUpdate(user, params){
     def updateEvent = new UserEventUpdate(user: user)
     updateEvent.populateFromUser()
-    updateEvent.properties = params['passwdRepeat','email','userRealName','description']
+    
+    if( params )
+      updateEvent.properties = params['passwdRepeat','email','userRealName','description']
+
     return updateEvent
   }
 
-  def create = {
-    [person: new User(params), authorityList: Role.list()]
-  }
+ 
 
-  private void addRoles(person) {
-    for (String key in params.keySet()) {
-      if (key.contains('ROLE') && 'on' == params.get(key)) {
-        Role.findByAuthority(key).addToPeople(person)
-      }
-    }
-  }
-
-  private Map buildPersonModel(person) {
-
-    List roles = Role.list()
-    roles.sort { r1, r2 ->
-      r1.authority <=> r2.authority
-    }
-    Set userRoleNames = []
-    for (role in person.authorities) {
-      userRoleNames << role.authority
-    }
-    LinkedHashMap<Role, Boolean> roleMap = [:]
-    for (role in roles) {
-      roleMap[(role)] = userRoleNames.contains(role.authority)
-    }
-
-    return [person: person, roleMap: roleMap]
-  }
 }
